5634a02417e5c03563634d2e2f28048ab246a181
dmitriy <fokinds1@gmail.com>
2020-08-24 16:55:55 +0600

    first commit. added empty project with necessary dependencies.

---
+++ src/main/java/ru/fds/hrdepartment/HrdepartmentApplication.java
@@ -0,0 +1,13 @@
+package ru.fds.hrdepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrdepartmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrdepartmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fds/hrdepartment/HrdepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fds.hrdepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrdepartmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
